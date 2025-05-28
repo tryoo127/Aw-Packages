266f9777b78af897b5be686daeda13c6c9526d51
tryoo127 <tryoo127@gmail.com>
2025-05-28 16:35:59 +0800

    Update qwrt-package.sh

--- qwrt-package.sh
+++ qwrt-package.sh
@@ -63,33 +63,6 @@ uci set dhcp.lan.dhcp_option='6,1.1.1.1,1.0.0.1'; uci commit dhcp.lan
 wget -q -O /etc/config/rirq "https://raw.githubusercontent.com/tryoo127/Aw-Packages/main/system/rirq";
 wget -q -O /etc/hotplug.d/iface/82-irqbalance "https://raw.githubusercontent.com/tryoo127/Aw-Packages/main/system/82-irqbalance";
 wget -q -O /etc/hotplug.d/net/97-smp-tune "https://raw.githubusercontent.com/tryoo127/Aw-Packages/main/system/97-smp-tune";
-clear
-
-clear
-echo -e "\e[1;37m[\e[0m \e[1;32mInstall Passwall Service\e[0m \e[1;37m]\e[0m"
-sleep 3
-
-clear
-echo "src/gz custom_packages https://github.com/NevermoreSSH/openwrt-packages2/releases/download/arca_presetv2" | tee -a /etc/opkg/customfeeds.conf;opkg update;opkg install luci-app-passwall htop haproxy;cd /tmp;curl -L https://github.com/mssvpn/Xray-core/releases/download/v1.7.2.1/Xray-linux-arm64-v8a.zip > Xray-linux-arm64-v8a.zip && unzip *.zip && mv xray /usr/bin && chmod +x /usr/bin/xray
-
-cat << 'EOF' > /etc/hotplug.d/iface/99-passwall
-#!/bin/sh
-
-log () {
- modlog "$@"
-}
-
-if [ "$ACTION" = "ifup" -a $INTERFACE = wan  -a $(uci -q get passwall.@global[0].enabled) -eq 1 ]; then
- sleep 10
- /etc/init.d/passwall restart
- if [ $? -eq 0 ]; then
-  log "Restart Passwall"
- else
-  log "failed to restart Passwall"
- fi
-fi
-done
-EOF
 
 clear
 echo -e "\e[1;37m[\e[0m \e[1;32mDownloading...Please Wait...\e[0m \e[1;37m]\e[0m"
