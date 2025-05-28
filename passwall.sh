a533c9b47a273f0af91a963ef38cbfb7bac2a21b
tryoo127 <tryoo127@gmail.com>
2025-05-28 14:13:41 +0800

    Add files via upload

--- passwall.sh
+++ passwall.sh
@@ -1,61 +1,71 @@
 #!/bin/sh
-clear
-echo -e "\e[0;37mInstall Passwall Qwrt By\e[0m \e[1;32m@XoolVPN\e[0m"
-sleep 3
-
-clear
-echo -e "\e[1;37m[\e[0m \e[1;32mInstallation Starts Now...\e[0m \e[1;37m]\e[0m"
-sleep 3
+installation() {
 echo "src/gz custom_packages https://github.com/NevermoreSSH/openwrt-packages2/releases/download/arca_presetv2" | tee -a /etc/opkg/customfeeds.conf;opkg update;opkg install luci-app-passwall htop haproxy;cd /tmp;curl -L https://github.com/mssvpn/Xray-core/releases/download/v1.7.2.1/Xray-linux-arm64-v8a.zip > Xray-linux-arm64-v8a.zip && unzip *.zip && mv xray /usr/bin && chmod +x /usr/bin/xray
-
-clear
-# Running Code 20%
-echo -ne '>>>>>>>>                   [20%]\r'
-sleep 2
-
-clear
-# Running Code 60%
-echo -ne '>>>>>>>>>                  [60%]\r'
-sleep 1
-
-cat << 'EOF' > /etc/hotplug.d/iface/99-passwall
-#!/bin/sh
-
-log () {
- modlog "$@"
+}
+progreSh() {
+    LR='\033[1;31m'
+    LG='\033[1;32m'
+    LY='\033[1;33m'
+    LC='\033[1;36m'
+    LW='\033[1;37m'
+    NC='\033[0m'
+    if [ "${1}" = "0" ]; then TME=$(date +"%s"); fi
+    SEC=`printf "%04d\n" $(($(date +"%s")-${TME}))`; SEC="$SEC sec"
+    PRC=`printf "%.0f" ${1}`
+    SHW=`printf "%3d\n" ${PRC}`
+    LNE=`printf "%.0f" $((${PRC}/2))`
+    LRR=`printf "%.0f" $((${PRC}/2-12))`; if [ ${LRR} -le 0 ]; then LRR=0; fi;
+    LYY=`printf "%.0f" $((${PRC}/2-24))`; if [ ${LYY} -le 0 ]; then LYY=0; fi;
+    LCC=`printf "%.0f" $((${PRC}/2-36))`; if [ ${LCC} -le 0 ]; then LCC=0; fi;
+    LGG=`printf "%.0f" $((${PRC}/2-48))`; if [ ${LGG} -le 0 ]; then LGG=0; fi;
+    LRR_=""
+    LYY_=""
+    LCC_=""
+    LGG_=""
+    for ((i=1;i<=13;i++))
+    do
+    	DOTS=""; for ((ii=${i};ii<13;ii++)); do DOTS="${DOTS}."; done
+    	if [ ${i} -le ${LNE} ]; then LRR_="${LRR_}#"; else LRR_="${LRR_}."; fi
+    	echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${DOTS}${LY}............${LC}............${LG}............ ${SHW}%${NC}\r"
+    	if [ ${LNE} -ge 1 ]; then sleep .05; fi
+    done
+    for ((i=14;i<=25;i++))
+    do
+    	DOTS=""; for ((ii=${i};ii<25;ii++)); do DOTS="${DOTS}."; done
+    	if [ ${i} -le ${LNE} ]; then LYY_="${LYY_}#"; else LYY_="${LYY_}."; fi
+    	echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${DOTS}${LC}............${LG}............ ${SHW}%${NC}\r"
+    	if [ ${LNE} -ge 14 ]; then sleep .05; fi
+    done
+    for ((i=26;i<=37;i++))
+    do
+    	DOTS=""; for ((ii=${i};ii<37;ii++)); do DOTS="${DOTS}."; done
+    	if [ ${i} -le ${LNE} ]; then LCC_="${LCC_}#"; else LCC_="${LCC_}."; fi
+    	echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${LC}${LCC_}${DOTS}${LG}............ ${SHW}%${NC}\r"
+    	if [ ${LNE} -ge 26 ]; then sleep .05; fi
+    done
+    for ((i=38;i<=49;i++))
+    do
+    	DOTS=""; for ((ii=${i};ii<49;ii++)); do DOTS="${DOTS}."; done
+    	if [ ${i} -le ${LNE} ]; then LGG_="${LGG_}#"; else LGG_="${LGG_}."; fi
+    	echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${LC}${LCC_}${LG}${LGG_}${DOTS} ${SHW}%${NC}\r"
+    	if [ ${LNE} -ge 38 ]; then sleep .05; fi
+    done
 }
 
-if [ "$ACTION" = "ifup" -a $INTERFACE = wan  -a $(uci -q get passwall.@global[0].enabled) -eq 1 ]; then
- sleep 10
- /etc/init.d/passwall restart
- if [ $? -eq 0 ]; then
-  log "Restart Passwall"
- else
-  log "failed to restart Passwall"
- fi
-fi
-done
-EOF
+printf "\n\n\n\n\n\n\n\n\n\n"
+progreSh 0
+progreSh 10
+progreSh 20
+progreSh 30
+progreSh 40
+progreSh 50
+progreSh 60
+progreSh 70
+progreSh 80
+progreSh 90
+progreSh 100
+printf "\n\n\n\n\n\n\n\n\n\n"
 
-clear
-echo -e "\e[1;37m[\e[0m \e[1;32mDownloading...Please Wait...\e[0m \e[1;37m]\e[0m"
-sleep 3
-
-clear
-# Running Code 80%
-echo -ne '>>>>>>>>>>>>>>             [80%]\r'
-sleep 1
-
-clear
-# Running Code 100%
-echo -ne '>>>>>>>>>>>>>>>>>>>>>>>>>>[100%]\r'
-sleep 3
-
-clear
-echo -ne "\e[1;37m[\e[0m \e[1;32mSuccessful!\e[0m \e[1;37m]\e[0m \e[0;37mReboot Now? (y/n)? : \e[0m"
-read answer
-if [ "$answer" == "${answer#[Yy]}" ] ;then
-exit 0
-else
-reboot
-fi
+# Start
+installation & progreSh
+exit
