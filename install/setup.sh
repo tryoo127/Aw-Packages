a9d91563df5cc370fb0fc89839cd91df020e5020
tryoo127 <tryoo127@gmail.com>
2025-05-29 20:30:09 +0800

    Add files via upload

--- install/setup.sh
+++ install/setup.sh
@@ -1,14 +1,34 @@
-#!/bin/bash
+#!/bin/sh
 clear
-echo -e "\e[0;37mAutoscript Oneliner QWRT By\e[0m \e[1;32mXoolVPN\e[0m"
+echo -ne "\e[0;37mAutoscript Oneliner QWRT By\e[0m \e[1;32mXoolVPN\e[0m"
 sleep 3
 
 clear
 #!/bin/sh
 fun_bar() {
+    CMD[0]="$1"
+    CMD[1]="$2"
+    (
+        [[ -e $HOME/fim ]] && rm $HOME/fim
+        ${CMD[0]} -y >/dev/null 2>&1
+        ${CMD[1]} -y >/dev/null 2>&1
+        touch $HOME/fim
+    ) >/dev/null 2>&1 &
     tput civis
-    echo -e "\e[1;32mPlease Wait\e[0m"
-    echo -e "\[1;32mDone!\e[0m"
+    echo -ne "\e[1;32mPlease Wait\e[0m \e[1;37m[\e[0m"
+    while true; do
+        for ((i = 0; i < 15; i++)); do
+            echo -ne "\033[0;32m>"
+            sleep 0.2s
+        done
+        [[ -e $HOME/fim ]] && rm $HOME/fim && break
+        echo -e "\033[0;33m]"
+        sleep 1s
+        tput cuu1
+        tput dl1
+        echo -ne "\e[1;32mPlease Wait\e[0m \e[1;37m[\e[0m"
+    done
+    echo -e "\e[1;37m] -\e[0m \033[1;32mDone!\033[1;37m"
     tput cnorm
     sleep 3s
 }
@@ -21,7 +41,7 @@ fun_bar 'sc1'
 
 rm -f /root/setup.sh
 clear
-echo -e "\e[1;37m[\e[0m \e[1;32mSuccessful!\e[0m \e[1;37m]\e[0m \e[0;37mReboot Now? (y/n)? : \e[0m"
+echo -ne "\e[1;37m[\e[0m \e[1;32mSuccessful!\e[0m \e[1;37m]\e[0m \e[0;37mReboot Now? (y/n)? : \e[0m"
 read answer
 if [ "$answer" == "${answer#[Yy]}" ] ;then
 exit 0
