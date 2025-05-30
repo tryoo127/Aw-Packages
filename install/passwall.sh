30d4a61070053c4abd33cb2f8f229762f3006600
tryoo127 <tryoo127@gmail.com>
2025-05-30 10:47:58 +0800

    Update passwall.sh

--- install/passwall.sh
+++ install/passwall.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 clear
-echo -e "\e[1;36m=============================================\e[0m"
-echo -e "\e[0;37m       Autoscript Passwall QWRT By \e[1;32m@XoolVPN\e[0m"
-echo -e "\e[1;36m=============================================\e[0m"
+echo -e "\e[0;37mAutoscript Passwall QWRT By \e[1;32m@XoolVPN\e[0m"
 echo
 sleep 2
 
@@ -10,12 +8,12 @@ sleep 2
 (echo "src/gz custom_packages https://github.com/NevermoreSSH/openwrt-packages2/releases/download/arca_presetv2" | tee -a /etc/opkg/customfeeds.conf > /dev/null 2>&1)
 sleep 1
 
-echo -n -e "\e[1;37m[ ] Downloading and installing Passwall service...\e[0m"
+echo -n -e "\e[1;37m[ ] Downloading and installing Passwall services...\e[0m"
 (opkg update > /dev/null 2>&1 && opkg install luci-app-passwall haproxy > /dev/null 2>&1)
 if [ $? -eq 0 ]; then
-    echo -e "\r\e[1;32m[✓] Downloading and installing Passwall service... Done!\e[0m"
+    echo -e "\r\e[1;32m[✓] Downloading and installing Passwall services... Done!\e[0m"
 else
-    echo -e "\r\e[1;31m[✗] Downloading and installing Passwall service... Failed!\e[0m"
+    echo -e "\r\e[1;31m[✗] Downloading and installing Passwall services... Failed!\e[0m"
 fi
 sleep 1
 
@@ -28,8 +26,8 @@ else
 fi
 sleep 1
 
-clear
-cat << 'EOF' > /etc/hotplug.d/iface/99-passwall
+echo -n -e "\e[1;37m[ ] Set autostart Passwall services...\e[0m"
+(cat << 'EOF' > /etc/hotplug.d/iface/99-passwall
 #!/bin/sh
 
 log () {
@@ -45,21 +43,25 @@ else
 log "failed to restart Passwall"
 fi
 fi
-done
 EOF
+)
+if [ $? -eq 0 ]; then
+    echo -e "\r\e[1;32m[✓] Set autostart Passwall services... Done!\e[0m"
+else
+    echo -e "\r\e[1;31m[✗] Set autostart Passwall services... Failed!\e[0m"
+fi
+sleep 1
 
 clear
 rm -f /root/passwall.sh
 
-echo -e "\n\e[1;36m=============================================\e[0m"
 echo -e "\e[1;37m[\e[0m \e[1;32mInstallation Successful!\e[0m \e[1;37m]\e[0m"
-echo -e "\e[1;36m=============================================\e[0m"
-echo -ne "\e[0;37mReboot Now to apply changes? (y/n): \e[0m"
+echo -ne "\e[0;37mReboot Now? (y/n): \e[0m"
 read answer
 if [ "$answer" == "${answer#[Yy]}" ] ;then
-    echo -e "\e[1;31mReboot skipped. Please reboot manually later for changes to take effect.\e[0m"
+    echo -e "\e[1;31mReboot skipped. Please Login QWRT web ui and refresh the page.\e[0m"
     exit 0
 else
-    echo -e "\e[1;32mRebooting system...\e[0m"
+    echo -e "\e[1;32mRebooting...\e[0m"
     reboot
 fi
