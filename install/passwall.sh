e0d97d41a7f58445fe7f70b83c87d76770f3e39b
tryoo127 <tryoo127@gmail.com>
2025-05-30 11:04:22 +0800

    Update passwall.sh

--- install/passwall.sh
+++ install/passwall.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 clear
 echo -e "\e[1;37mAutoscript Passwall QWRT By \e[1;32m@XoolVPN\e[0m"
-echo
 sleep 2
 
 #Installation Starts
@@ -55,12 +54,12 @@ sleep 3
 clear
 rm -f /root/passwall.sh
 
-echo -ne "\e[1;37m[\e[0m \e[1;32mSuccessful!\e[0m \e[1;37m]\e[0m \e[0;37mReboot Now? (y/n): \e[0m""
+echo -ne "\e[1;37m[\e[0m \e[1;32mSuccessful!\e[0m \e[1;37m]\e[0m \e[0;37mReboot Now? (y/n)? : \e[0m"
 read answer
 if [ "$answer" == "${answer#[Yy]}" ] ;then
-echo -e "\e[1;31mReboot skipped. Please Login QWRT web ui and refresh the page.\e[0m"
-exit 0
+    echo -e "\e[1;31mReboot skipped. Please Login QWRT web ui and refresh the page.\e[0m"
+    exit 0
 else
-echo -e "\e[1;32mRebooting...\e[0m"
-reboot
+    echo -e "\e[1;32mRebooting...\e[0m"
+    reboot
 fi
