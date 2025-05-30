efaf5b5c840c46456a666fb00d1f58dbd3237ae9
tryoo127 <tryoo127@gmail.com>
2025-05-30 21:59:32 +0800

    Update package.sh

--- install/package.sh
+++ install/package.sh
@@ -5,12 +5,10 @@ echo -e "\e[1;37m     Installer Package QWRT By \e[1;32m@XoolVPN\e[0m"
 echo -e "\e[1;36m=============================================\e[0m"
 sleep 2
 
-#Installation Starts
 execute_and_check() {
     local cmd="$1"
     local message="$2"
     echo -n -e "\e[1;37m$message...\e[0m"
-    # Run command in background and redirect output to /dev/null
     eval "$cmd" > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo -e "\r\e[1;37m$message...\e[0m\e[1;32mDone!\e[0m"
@@ -19,11 +17,9 @@ execute_and_check() {
     fi
 }
 
-# Step 1: Configure /etc/rc.local for TTL settings
 execute_and_check "rm -f /etc/rc.local && cat > /etc/rc.local <<-'RCD'
 #!/bin/sh -e
 
-# Apply TTL settings to modify network packets
 iptables -t mangle -I POSTROUTING -o wwan0 -j TTL --ttl-set 64
 iptables -t mangle -I POSTROUTING -o br-lan -j TTL --ttl-set 64
 iptables -t mangle -I PREROUTING -i wwan0 -j TTL --ttl-set 64
@@ -38,18 +34,16 @@ exit 0
 RCD
 chmod +x /etc/rc.local && /etc/rc.local enable && /etc/rc.local start && /etc/rc.local restart" "Setting up TTL modifications"
 
-# Step 2: Configure language and System Timezone
 execute_and_check "uci set luci.main.lang='en'; uci commit" "Setting language to English"
-execute_and_check "uci set system.@system[0].zonename='Asia/Kuala Lumpur'; uci commit system" "Setting system timezone to Asia/Kuala Lumpur"
-
-# Step 3: Configure NTP server
+execute_and_check "uci set system.@system[0].zonename='Asia/Kuala Lumpur'; uci commit system" "Setting system timezone Asia/Kuala Lumpur"
 execute_and_check "uci -q delete system.ntp.server; uci add_list system.ntp.server='time.cloudflare.com'; uci commit system.ntp; /etc/init.d/sysntpd restart" "Configuring and restarting NTP service"
-
-# Step 4: Configure WAN interface names
 execute_and_check "uci set network.wan.ifname='wwan0_1'; uci commit network.wan" "Setting WAN interface network"
 execute_and_check "uci set network.wan6.ifname='wwan0_1'; uci commit network.wan6" "Setting WAN6 interface network"
-
-# Step 5: Download and update Modem interface files
+execute_and_check "uci set turboacc.config.bbr_cca='1'; uci commit turboacc.config" "Set CPU modem performance"
+execute_and_check "uci set turboacc.config.dns_caching='1'; uci set turboacc.config.dns_caching_dns='1.1.1.1,1.0.0.1'; uci commit turboacc.config" "Set DNS & enable BBR CCA"
+execute_and_check "uci set cpufreq.cpufreq.governor='performance'; uci commit cpufreq.cpufreq" "Set CPU to maximum frequency"
+uci set cpufreq.cpufreq.minifreq='2208000'; uci commit cpufreq.cpufreq
+uci set dhcp.lan.dhcp_option='6,1.1.1.1,1.0.0.1'; uci commit dhcp.lan
 echo -e "\r\e[1;37mDownloading Modem interface files...\e[0m\e[1;32mDone!\e[0m"
 wget -q -O /usr/lib/lua/luci/model/cbi/rooter/customize.lua "https://raw.githubusercontent.com/tryoo127/Aw-Packages/main/system/customize.lua" &> /dev/null
 wget -q -O /usr/lib/lua/luci/model/cbi/rooter/profiles.lua "https://raw.githubusercontent.com/tryoo127/Aw-Packages/main/system/profiles.lua" &> /dev/null
@@ -65,13 +59,6 @@ wget -q -O /usr/lib/lua/luci/view/modlog/modlog.htm "https://raw.githubuserconte
 wget -q -O /etc/config/rirq "https://raw.githubusercontent.com/tryoo127/Aw-Packages/main/system/rirq";
 wget -q -O /etc/hotplug.d/iface/82-irqbalance "https://raw.githubusercontent.com/tryoo127/Aw-Packages/main/system/82-irqbalance";
 wget -q -O /etc/hotplug.d/net/97-smp-tune "https://raw.githubusercontent.com/tryoo127/Aw-Packages/main/system/97-smp-tune";
-
-# Step 1: Configure /etc/rc.local for TTL settings
-execute_and_check "uci set turboacc.config.bbr_cca='1'; uci commit turboacc.config" "Set CPU modem performance"
-execute_and_check "uci set turboacc.config.dns_caching='1'; uci set turboacc.config.dns_caching_dns='1.1.1.1,1.0.0.1'; uci commit turboacc.config" "Set DNS & enable BBR CCA"
-execute_and_check "uci set cpufreq.cpufreq.governor='performance'; uci commit cpufreq.cpufreq" "Set CPU to maximum frequency"
-uci set cpufreq.cpufreq.minifreq='2208000'; uci commit cpufreq.cpufreq
-uci set dhcp.lan.dhcp_option='6,1.1.1.1,1.0.0.1'; uci commit dhcp.lan
 echo -e "\e[1;37mPlease wait...\e[0m"
 sleep 3
 
