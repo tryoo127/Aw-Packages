b888cd9b9fb74f8ae34862690e2b97b8d669b337
tryoo127 <tryoo127@gmail.com>
2025-05-30 14:39:47 +0800

    Update oneliner.sh

--- install/oneliner.sh
+++ install/oneliner.sh
@@ -37,21 +37,21 @@ iptables -t mangle -I PREROUTING -i wwan0_1 -j TTL --ttl-set 64
 iptables -t mangle -I PREROUTING -i br-lan -j TTL --ttl-set 64
 exit 0
 RCD
-chmod +x /etc/rc.local && /etc/rc.local enable && /etc/rc.local start && /etc/rc.local restart" "Setting up TTL modifications."
+chmod +x /etc/rc.local && /etc/rc.local enable && /etc/rc.local start && /etc/rc.local restart" "Setting up TTL modifications"
 
-# Step 2: Configure LuCI language and System Timezone
+# Step 2: Configure language and System Timezone
 execute_and_check "uci set luci.main.lang='en'; uci commit" "Setting language to English"
 execute_and_check "uci set system.@system[0].zonename='Asia/Kuala Lumpur'; uci commit system" "Setting system timezone to Asia/Kuala Lumpur"
 
 # Step 3: Configure NTP server
-execute_and_check "uci -q delete system.ntp.server; uci add_list system.ntp.server='time.cloudflare.com'; uci commit system.ntp; /etc/init.d/sysntpd restart" "Configuring and restarting NTP with Cloudflare"
+execute_and_check "uci -q delete system.ntp.server; uci add_list system.ntp.server='time.cloudflare.com'; uci commit system.ntp; /etc/init.d/sysntpd restart" "Configuring and restarting NTP service"
 
 # Step 4: Configure WAN interface names
-execute_and_check "uci set network.wan.ifname='wwan0_1'; uci commit network.wan" "Setting WAN interface to wwan0_1"
-execute_and_check "uci set network.wan6.ifname='wwan0_1'; uci commit network.wan6" "Setting WAN6 interface to wwan0_1"
+execute_and_check "uci set network.wan.ifname='wwan0_1'; uci commit network.wan" "Setting WAN interface network"
+execute_and_check "uci set network.wan6.ifname='wwan0_1'; uci commit network.wan6" "Setting WAN6 interface network"
 
-# Step 5: Download and update LuCI web interface files
-echo -e "\r\e[1;37mDownloading LuCI web interface files...\e[0m\e[1;32mDone!\e[0m"
+# Step 5: Download and update Modem interface files
+echo -e "\r\e[1;37mDownloading Modem interface files...\e[0m\e[1;32mDone!\e[0m"
 wget -q -O /usr/lib/lua/luci/model/cbi/rooter/customize.lua "https://raw.githubusercontent.com/tryoo127/Aw-Packages/main/system/customize.lua" &> /dev/null
 wget -q -O /usr/lib/lua/luci/model/cbi/rooter/profiles.lua "https://raw.githubusercontent.com/tryoo127/Aw-Packages/main/system/profiles.lua" &> /dev/null
 wget -q -O /usr/lib/lua/luci/controller/admin/modem.lua "https://raw.githubusercontent.com/tryoo127/Aw-Packages/main/system/modem.lua" &> /dev/null
@@ -65,14 +65,14 @@ wget -q -O /usr/lib/lua/luci/view/rooter/sms.htm "https://raw.githubusercontent.
 wget -q -O /usr/lib/lua/luci/view/modlog/modlog.htm "https://raw.githubusercontent.com/tryoo127/Aw-Packages/main/system/modlog.htm" &> /dev/null
 echo
 echo -e "\e[1;37mPlease wait...\e[0m"
-sleep 5
+sleep 3
 
 clear
 rm -f /root/oneliner.sh
 echo -ne "\e[1;37m[\e[0m \e[1;32mSuccessful!\e[0m \e[1;37m]\e[0m \e[0;37mReboot Now? (y/n)? : \e[0m"
 read answer
 if [ "$answer" == "${answer#[Yy]}" ] ;then
-    echo -e "\e[1;31mReboot skipped. Please reboot manually later for changes to take effect.\e[0m"
+    echo -e "\e[1;31mReboot skipped. Please reboot manually for changes to take effect.\e[0m"
     exit 0
 else
     echo -e "\e[1;32mRebooting...\e[0m"
