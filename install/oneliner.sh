1b68606051fc2e0e007ca0a8c96629253767d573
tryoo127 <tryoo127@gmail.com>
2025-06-01 17:43:01 +0800

    Update oneliner.sh

--- install/oneliner.sh
+++ install/oneliner.sh
@@ -6,12 +6,10 @@ echo -e "\e[1;36m=============================================\e[0m"
 echo
 sleep 2
 
-#Installation Starts
 execute_and_check() {
     local cmd="$1"
     local message="$2"
     echo -n -e "\e[1;37m$message...\e[0m"
-    # Run command in background and redirect output to /dev/null
     eval "$cmd" > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo -e "\r\e[1;37m$message...\e[0m\e[1;32mDone!\e[0m"
@@ -20,11 +18,10 @@ execute_and_check() {
     fi
 }
 
-# Step 1: Configure /etc/rc.local for TTL settings
-execute_and_check "rm -f /etc/rc.local && cat > /etc/rc.local <<-'RCD'
+rm -rf /etc/rc.local
+cat > /etc/rc.local <<-RCD
 #!/bin/sh -e
 
-# Apply TTL settings to modify network packets
 iptables -t mangle -I POSTROUTING -o wwan0 -j TTL --ttl-set 64
 iptables -t mangle -I POSTROUTING -o br-lan -j TTL --ttl-set 64
 iptables -t mangle -I PREROUTING -i wwan0 -j TTL --ttl-set 64
@@ -37,21 +34,17 @@ iptables -t mangle -I PREROUTING -i wwan0_1 -j TTL --ttl-set 64
 iptables -t mangle -I PREROUTING -i br-lan -j TTL --ttl-set 64
 exit 0
 RCD
-chmod +x /etc/rc.local && /etc/rc.local enable && /etc/rc.local start && /etc/rc.local restart" "Setting up TTL modifications"
+chmod +x /etc/rc.local
+/etc/rc.local enable
+/etc/rc.local start
+/etc/rc.local restart
 
-# Step 2: Configure language and System Timezone
+execute_and_check "opkg update; opkg install htop; opkg install vsftpd" "- Update all packages. Please wait"
 execute_and_check "uci set luci.main.lang='en'; uci commit" "Setting language to English"
 execute_and_check "uci set system.@system[0].zonename='Asia/Kuala Lumpur'; uci commit system" "Setting system timezone to Asia/Kuala Lumpur"
-
-# Step 3: Configure NTP server
 execute_and_check "uci -q delete system.ntp.server; uci add_list system.ntp.server='time.cloudflare.com'; uci commit system.ntp; /etc/init.d/sysntpd restart" "Configuring and restarting NTP service"
-
-# Step 4: Configure WAN interface names
 execute_and_check "uci set network.wan.ifname='wwan0_1'; uci commit network.wan" "Setting WAN interface network"
 execute_and_check "uci set network.wan6.ifname='wwan0_1'; uci commit network.wan6" "Setting WAN6 interface network"
-
-# Step 5: Download and update Modem interface files
-echo -e "\r\e[1;37mDownloading Modem interface files...\e[0m\e[1;32mDone!\e[0m"
 wget -q -O /usr/lib/lua/luci/model/cbi/rooter/customize.lua "https://raw.githubusercontent.com/tryoo127/Aw-Packages/main/system/customize.lua" &> /dev/null
 wget -q -O /usr/lib/lua/luci/model/cbi/rooter/profiles.lua "https://raw.githubusercontent.com/tryoo127/Aw-Packages/main/system/profiles.lua" &> /dev/null
 wget -q -O /usr/lib/lua/luci/controller/admin/modem.lua "https://raw.githubusercontent.com/tryoo127/Aw-Packages/main/system/modem.lua" &> /dev/null
@@ -63,12 +56,10 @@ wget -q -O /usr/lib/lua/luci/view/rooter/custom.htm "https://raw.githubuserconte
 wget -q -O /usr/lib/lua/luci/view/rooter/net_status.htm "https://raw.githubusercontent.com/tryoo127/Aw-Packages/main/system/net_status.htm" &> /dev/null
 wget -q -O /usr/lib/lua/luci/view/rooter/sms.htm "https://raw.githubusercontent.com/tryoo127/Aw-Packages/main/system/sms.htm" &> /dev/null
 wget -q -O /usr/lib/lua/luci/view/modlog/modlog.htm "https://raw.githubusercontent.com/tryoo127/Aw-Packages/main/system/modlog.htm" &> /dev/null
-echo
-echo -e "\e[1;37mPlease wait...\e[0m"
 sleep 3
 
-clear
 rm -f /root/oneliner.sh
+echo
 echo -ne "\e[1;37m[\e[0m \e[1;32mSuccessful!\e[0m \e[1;37m]\e[0m \e[0;37mReboot Now? (y/n)? : \e[0m"
 read answer
 if [ "$answer" == "${answer#[Yy]}" ] ;then
